ab2791083048bc3c71dad922f630c014cbc78c1b
VyperiumVictor13 <vyperium.victor@gmail.com>
2021-11-07 19:41:28 -0500

    Definição da Firebase.

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,12 @@
 const firebase = require('firebase');
 let firebaseConfig = {
-  //firebase keys
+    apiKey: "AIzaSyD1x-ZlSqNp8JMsFfx1yP2JbL58mxbBUAY",
+    authDomain: "projeto-v9-3400e.firebaseapp.com",
+    projectId: "projeto-v9-3400e",
+    storageBucket: "projeto-v9-3400e.appspot.com",
+    messagingSenderId: "437513431536",
+    appId: "1:437513431536:web:c5ef7a70b80f260c3a5535",
+    measurementId: "G-MSB58XD76C"
 };
 firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
+var firebase = firebase.firestore();
